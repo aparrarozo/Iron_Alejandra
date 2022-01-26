52cd2fe81296ad25655efc4c22200d34052051cc
Alejandra <aleja.pr@gmail.com>
2022-01-26 14:35:06 +0100

    Lab Queries & Lab Subqueries (name changes)

---
+++ Lab SQL/Lab SQL Subq2 .sql
@@ -0,0 +1,10 @@
+-- 1 How many copies of the film Hunchback Impossible exist in the inventory system?
+-- 2 List all films whose length is longer than the average of all the films.
+-- 3 Use subqueries to display all actors who appear in the film Alone Trip.
+-- 4 Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
+-- 5 Get name and email from customers from Canada using subqueries. 
+-- 	Do the same with joins. 
+-- Note that to create a join, you will have to identify the correct tables with their primary keys and foreign keys, 
+-- that will help you get the relevant information.
+
+use sakila
